38f5999a29e2723d9768575e68a45c4f395ae4ac
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-08-29 17:28:03 -0300

    agrego modificar idioma y skills

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function checkStickyNav() {
 window.addEventListener('scroll', checkStickyNav)
 
 //-------------------------
-
+// TEXTO
 document.addEventListener("DOMContentLoaded", function () {
 	const typingElement = document.querySelector(".typing-effect")
 	const text = typingElement.getAttribute("data-text")
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 
 // ------------------------------------------------------
-
+// SCROLL
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggle-menu')
     const mobileMenu = document.getElementById('mobile-menu')
@@ -51,3 +51,58 @@ document.addEventListener('DOMContentLoaded', function() {
         mobileMenu.classList.toggle('active');
     })
 })
+
+// ------------------------------------------------------
+// SANITY
+// import sanityClient from '@sanity/client';
+
+// const client = sanityClient({
+//   projectId: process.env.SANITY_PROJECT_ID,
+//   dataset: process.env.SANITY_DATASET,
+//   token: process.env.SANITY_TOKEN,
+//   useCdn: true
+// })
+
+// ------------------------------------------------------
+// IDIOMAS
+
+const changeLanguage = async language => {
+    const requestJson = await fetch(`./languages/${language}.json`)
+    const text = await requestJson.json()
+
+    for(const textToChange of textChange) {
+        const section = textToChange.dataset.section
+        const value = textToChange.dataset.value
+
+        if (textToChange.tagName === "INPUT") {
+            textToChange.placeholder = text[section][value]
+        } else {
+            textToChange.innerText = text[section][value]
+        }
+    }
+
+    localStorage.setItem('language', language)
+}
+
+const flagsBtn = document.getElementById('flags')
+const textChange = document.querySelectorAll("[data-section]")
+
+const savedLanguage = localStorage.getItem('language')
+if (savedLanguage) {
+    changeLanguage(savedLanguage)
+    const activeButton = document.querySelector(`[data-language="${savedLanguage}"]`)
+    activeButton.classList.add('active')
+}
+
+flagsBtn.addEventListener('click', (e) => {
+    console.log("click")
+    const newLanguage = e.target.parentElement.dataset.language
+    changeLanguage(newLanguage)
+
+    flagsBtn.querySelectorAll('.flags-item').forEach(item => {
+        item.classList.remove('active')
+    })
+    e.target.parentElement.classList.add('active')
+})
+
+
