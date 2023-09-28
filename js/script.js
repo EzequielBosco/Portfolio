d07d7481d2f9b49055a9202310e50fdb1e457c0d
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-09-28 00:11:57 -0300

    configuro contact-form con firebase, agrego efecto fade, switch para idiomas

--- js/script.js
+++ js/script.js
@@ -32,19 +32,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const savedLanguage = localStorage.getItem("language")
 
     if (!savedLanguage) {
+        
         currentText = ""
         let i = 0
         const typingInterval = setInterval(function () {
-          currentText += text.charAt(i)
-          typingElement.innerText += currentText.charAt(i)
-          i++
-          if (i >= text.length) {
-            clearInterval(typingInterval)
-          }
-        }, 100)
-      } else {
-        changeLanguage(savedLanguage)
-      }
+            currentText += text.charAt(i)
+            typingElement.innerText += currentText.charAt(i)
+            i++
+            if (i >= text.length) {
+                clearInterval(typingInterval)
+            }}, 100)
+        } else {
+            changeLanguage(savedLanguage)
+    }
 })
 
 // ------------------------------------------------------
@@ -70,8 +70,8 @@ document.addEventListener('DOMContentLoaded', function() {
 // })
 
 // ------------------------------------------------------
-// IDIOMAS
 
+// IDIOMAS
 const changeLanguage = async language => {
     const requestJson = await fetch(`./languages/${language}.json`)
     const text = await requestJson.json()
@@ -90,25 +90,90 @@ const changeLanguage = async language => {
     localStorage.setItem('language', language)
 }
 
-const flagsBtn = document.getElementById('flags')
+const switchInput = document.getElementById("switch")
+const switchLabel = document.querySelector(".checkbox-div label")
 const textChange = document.querySelectorAll("[data-section]")
+
+const savedSwitchState = localStorage.getItem('languageSwitchState')
+if (savedSwitchState !== null) {
+    switchInput.checked = savedSwitchState === "true";
+}
+
+const switchState = switchInput.checked
+localStorage.setItem('languageSwitchState', switchState.toString())
+
+// Cambio de estado
+switchInput.addEventListener("change", () => {
+    const newLanguage = switchInput.checked ? "es" : "en"
+    changeLanguage(newLanguage)
+
+    localStorage.setItem('languageSwitchState', switchInput.checked.toString())
+
+    // Cambiar el texto
+    const switchXText = document.querySelector(".switch-x-text")
+    switchXText.textContent = `${newLanguage.toUpperCase()}` + " "
+})
 
 const savedLanguage = localStorage.getItem('language')
 if (savedLanguage) {
     changeLanguage(savedLanguage)
-    const activeButton = document.querySelector(`[data-language="${savedLanguage}"]`)
-    activeButton.classList.add('active')
 }
 
-flagsBtn.addEventListener('click', (e) => {
-    console.log("click")
-    const newLanguage = e.target.parentElement.dataset.language
-    changeLanguage(newLanguage)
-
-    flagsBtn.querySelectorAll('.flags-item').forEach(item => {
-        item.classList.remove('active')
-    })
-    e.target.parentElement.classList.add('active')
+// EFECTO
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.body.classList.remove('fade')
+    }, 600)
 })
 
 
+// CONTACT FORM
+const firebaseConfig = {
+    apiKey: "AIzaSyCbbkJbEfF3wN5KTtB6thtEiw22e7sc368",
+    authDomain: "portfolio-contacto-c3ee7.firebaseapp.com",
+    databaseURL: "https://portfolio-contacto-c3ee7-default-rtdb.firebaseio.com",
+    projectId: "portfolio-contacto-c3ee7",
+    storageBucket: "portfolio-contacto-c3ee7.appspot.com",
+    messagingSenderId: "798531995796",
+    appId: "1:798531995796:web:b8253700a6d9eeec008c81"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const contactFormDB = firebase.database().ref('PortfolioContactForm')
+
+const contactForm = document.getElementById('contact-form')
+contactForm.addEventListener('submit', submitForm)
+
+function submitForm(e) {
+    e.preventDefault()
+
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const message = document.getElementById('message').value
+
+    saveMessages(name, email, message)
+    alert()
+
+    const form = e.target
+    form.reset()
+}
+
+const saveMessages = (name, email, message) => {
+    const newContactForm = contactFormDB.push()
+    
+    newContactForm.set({
+        name: name,
+        email: email,
+        message: message
+    })
+}
+
+// ALERT
+const alert = () => {
+    Swal.fire({
+        icon: 'success',
+        title: 'Enviado',
+        showCancelButton: false,
+    })
+}
